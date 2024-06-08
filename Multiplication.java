dbeae94a6b177c87d02dd58dd7a17cecd04320c0
sridharanish19 <sridharanish19@gmail.com>
2024-06-08 14:54:00 -0500

    Create Multiplication.java
    Multiplication

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 30;
+        System.out.println("Number1: "+num1);
+        System.out.println("Number2: "+num2);
+        int result = num1 * num2;
+        System.out.println("Multiplication of 2 numbers is: "+result);
+    }
+}
